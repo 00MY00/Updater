67a458b94d79da4da5d717698061e3f6c594c641
00MY00 <75865145+00MY00@users.noreply.github.com>
2023-01-09 08:58:07 +0100

    Update WifiPumpkin3.sh

--- WifiPumpkin3.sh
+++ WifiPumpkin3.sh
@@ -6,7 +6,7 @@ dpkg --configure -a
 apt install libssl-dev libffi-dev build-essential -y
 
 git clone https://github.com/P0cL4bs/wifipumpkin3.git  
-cd wifipumpkin3
+cd ./wifipumpkin3/
 apt install python3-pyqt5
 
 python3 -c "from PyQt5.QtCore import QSettings; print('done')"
