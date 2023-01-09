f597457d7ae649c87a7d03016a659adc9a4bfaa3
00MY00 <75865145+00MY00@users.noreply.github.com>
2023-01-09 14:18:23 +0100

    Update WifiPumpkin3.sh

--- WifiPumpkin3.sh
+++ WifiPumpkin3.sh
@@ -16,7 +16,43 @@ python3 -c "from PyQt5.QtCore import QSettings; print('done')"
 
 apt install hostapd -y
 
-wifipumpkin3
+rm -f ./NoProxy.pulp
+rm -f ./whidLoginProxy.pulp
+
+
+clear
+echo
+echo choisiser le type de Wi-Fi
+echo
+echo [1] normal sens Proxy
+echo [2] Wi-Fi avec page de login
+echo
+read -p ": " user
+
+if [ "$user" = "1" ]:
+then
+  echo # Creating regulard Wifi point >> NoProxy.pulp
+  echo set interface wlan0 >> NoProxy.pulp
+  echo set ssid Free Wifi >> NoProxy.pulp
+  echo set proxy noproxy >> NoProxy.pulp
+  echo ignore pydns_server >> NoProxy.pulp
+  echo start >> NoProxy.pulp
+  wifipumpkin3 --pulp ./NoProxy.pulp
+
+elif [ "$user" = "2" ]:
+then
+  echo # Creating Wifi point Whid web Login page  >> whidLoginProxy.pulp
+  echo set interface wlan0 >> whidLoginProxy.pulp
+  echo set ssid Free Wifi >> whidLoginProxy.pulp
+  echo set proxy captiveflask >> whidLoginProxy.pulp
+  echo ignore pydns_server >> whidLoginProxy.pulp
+  echo start >> whidLoginProxy.pulp 
+  
+  wifipumpkin3 --pulp ./whidLoginProxy.pulp
+
+fi
+
+
 
 ######################
 # Creating Wifi point
@@ -36,10 +72,6 @@ wifipumpkin3
 # set proxy captiveflask
 # ignore pydns_server
 # start
-
-
-
-
 
 #######################################
 # Wifipumpkin3 multi AP deauth attack #
